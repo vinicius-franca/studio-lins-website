eaa9e4bae6ffd0f88d586a15626d7a8b5dc10031
Vinicius França <vinicius.franca@meta.com.br>
2021-06-21 16:56:10 -0300

    fix form

--- src/pages/contato/index.js
+++ src/pages/contato/index.js
@@ -26,9 +26,10 @@ export default class Index extends React.Component {
     const form = e.target
     fetch('/', {
       method: 'POST',
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({
         'form-name': form.getAttribute('name'),
-        ...this.state,
+        ...this.state
       }),
     })
     .then(() => alert("Success!"))
