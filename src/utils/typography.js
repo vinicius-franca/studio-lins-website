015b3087954c86172789efcf36f0de1f88abff7a
Vinicius França <vinicius.franca@meta.com.br>
2021-06-28 21:45:54 -0300

    fix typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,7 +3,7 @@ import Typography from "typography";
 const typography = new Typography({
   baseFontSize: "18px",
   baseLineHeight: 1.45,
-  bodyFontFamily: ['Poiret One', cursive],
+  bodyFontFamily: ['Poiret One', 'sans-serif'],
 });
 
 export default typography;
